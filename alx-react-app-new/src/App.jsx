523b5a543c6041d10819e66e99809f52e1f5ac0d
Salah535858 <salah.bakali4@gmail.com>
2024-11-10 15:50:52 +0100

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,30 +1,16 @@
 // src/App.jsx
 import React from 'react';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
-import WelcomeMessage from './components/WelcomeMessage'; // Ensure the path is correct
-import Counter from './components/Counter'; // Import the Counter component
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext'; // Import the UserContext
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-    <div className="App" style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome to My React App!</h1>
-      <WelcomeMessage /> {/* Display the WelcomeMessage component */}
-      
-      {/* The rest of the components */}
-      <Header />
-      <MainContent />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Footer />
-      
-      {/* Add the Counter component */}
-      <Counter />
-    </div>
+    <UserContext.Provider value={userData}> {/* Provide the context value */}
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
 export default App;
-
-
