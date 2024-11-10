e25a5086e31343062c61a132367c58359409054c
Salah535858 <salah.bakali4@gmail.com>
2024-11-10 14:39:31 +0100

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 
 function MainContent() {
     return (
-        <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
-        </main>
+<main style={{ padding: '20px', backgroundColor: 'lightgray' }}>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+
+<footer style={{ backgroundColor: 'navy', color: 'white', padding: '10px', textAlign: 'center' }}>
+  <p>Footer content here</p>
+</footer>
+
     );
 }
 
