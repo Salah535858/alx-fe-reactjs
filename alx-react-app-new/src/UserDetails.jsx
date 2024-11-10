957be96f84e475fa92dd7203a942b2780e3782a4
Salah535858 <salah.bakali4@gmail.com>
2024-11-10 15:55:31 +0100

    Create UserDetails.jsx

---
+++ alx-react-app-new/src/UserDetails.jsx
@@ -0,0 +1,12 @@
+// src/UserInfo.jsx
+import React, { useContext } from 'react';
+import UserDetails from './UserDetails';
+import UserContext from './UserContext'; // Import UserContext
+
+function UserInfo() {
+  const userData = useContext(UserContext); // Consume the context
+
+  return <UserDetails userData={userData} />;
+}
+
+export default UserInfo;
