960ab8f049e61a3c3251233f0415b0611c7bffb7
Salah535858 <salah.bakali4@gmail.com>
2024-11-26 17:02:14 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserProfile from "./components/UserProfile"; // Import the UserProfile component
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <UserProfile /> {/* Render the UserProfile component */}
+    </div>
+  );
+};
+
+export default App;
